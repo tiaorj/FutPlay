9c74d17080c02623abb2acb215dc40c38580b8ad
tiaorj <tiaorj@gmail.com>
2026-05-18 10:10:16 -0300

    Atualiza marca publica para Meu Pitaco FC

---
+++ Settings/AppBrand.cs
@@ -0,0 +1,10 @@
+﻿namespace FutPlay.Settings
+{
+    public static class AppBrand
+    {
+        public const string Name = "Meu Pitaco FC";
+        public const string Slogan = "Seu bolão de futebol online";
+        public const string Description = "Crie ligas, convide amigos, dê seus palpites e dispute o ranking.";
+        public const string Domain = "meupitacofc.com.br";
+    }
+}
